209e810382913625d9d273303206c2e2ea7db1eb
masaki.ono <aaikmnoos@gmail.com>
2022-10-29 17:46:43 +0900

    Fix bug

--- cmake/deps.cmake
+++ cmake/deps.cmake
@@ -1,12 +1,16 @@
 if(LCV_BUILD_CXXOPTS)
-    include(FetchContent)
-    FetchContent_Declare(cxxopts GIT_REPOSITORY https://github.com/jarro2783/cxxopts.git)
-    FetchContent_MakeAvailable(cxxopts)
+  include(FetchContent)
+  FetchContent_Declare(
+    cxxopts
+    GIT_REPOSITORY https://github.com/jarro2783/cxxopts.git
+    GIT_TAG v3.0.0)
+  FetchContent_MakeAvailable(cxxopts)
 else()
-    find_package(cxxopts REQUIRED)
+  find_package(cxxopts REQUIRED)
 endif()
 
-if(LCV_BUILD_GTEST)
+if(LCV_BUILD_TEST)
+  if(LCV_BUILD_GTEST)
     include(FetchContent)
     FetchContent_Declare(googletest # Specify the commit you depend on and update it regularly.
                          URL https://github.com/google/googletest/archive/5376968f6948923e2411081fd9372e71a59d8e77.zip)
@@ -14,11 +18,13 @@ if(LCV_BUILD_GTEST)
         ON
         CACHE BOOL "" FORCE)
     FetchContent_MakeAvailable(googletest)
-    include(GoogleTest)
-else()
-    find_package(GTest REQUIRED)
+  else()
+    find_package(GTest CONFIG REQUIRED)
+    set_target_properties(GTest::gtest PROPERTIES IMPORTED_GLOBAL TRUE)
+    set_target_properties(GTest::gtest_main PROPERTIES IMPORTED_GLOBAL TRUE)
     add_library(gtest ALIAS GTest::gtest)
     add_library(gtest_main ALIAS GTest::gtest_main)
+  endif()
 endif()
 
 find_package(OpenCV REQUIRED)
