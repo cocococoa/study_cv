2175a971e85ec22c3105ab9000bcbbb274923da7
masaki.ono <aaikmnoos@gmail.com>
2022-10-29 20:30:55 +0900

    Use filesystem

--- apps/imread.cpp
+++ apps/imread.cpp
@@ -1,9 +1,11 @@
 #include <cxxopts.hpp>
-#include <opencv2/opencv.hpp>
+#include <filesystem>
+#include <opencv2/highgui/highgui.hpp>
 
 int main(int argc, char** argv) {
-  auto options = cxxopts::Options("imread", "OpenCV imread");
   auto img_path = std::string();
+
+  auto options = cxxopts::Options("imread", "OpenCV imread");
   options.add_options()
       // help
       ("h,help", "Print help")
@@ -11,12 +13,19 @@ int main(int argc, char** argv) {
       ("i", "Input path", cxxopts::value<std::string>(img_path));
   const auto result = options.parse(argc, argv);
   if (argc == 1 || result.count("help") >= 1) {
-    std::cout << options.help({""});
+    std::cerr << options.help({""});
+    return -1;
+  }
+  if (!std::filesystem::exists(std::filesystem::status(img_path))) {
+    std::cerr << "No such file: " << img_path << std::endl;
     return -1;
   }
 
   auto img = cv::imread(img_path, -1);
-  if (img.empty()) return -1;
+  if (img.empty()) {
+    std::cerr << "Failed to open image" << std::endl;
+    return -1;
+  }
 
   cv::namedWindow("imread", cv::WINDOW_NORMAL);
   cv::imshow("imread", img);
