d4d4ff6ce8e48e2765c972de8ef9a74dd2dbb271
masaki.ono <aaikmnoos@gmail.com>
2022-10-29 17:55:08 +0900

    Add cxxopts

--- apps/imread.cpp
+++ apps/imread.cpp
@@ -1,10 +1,22 @@
+#include <cxxopts.hpp>
 #include <opencv2/opencv.hpp>
 
-#include <cxxopts.h>
-
 int main(int argc, char** argv) {
-    auto img = cv::imread(argv[1], -1);
-    if (img.empty()) return -1;
+  auto options = cxxopts::Options("imread", "OpenCV imread");
+  auto img_path = std::string();
+  options.add_options()
+      // help
+      ("h,help", "Print help")
+      // input path
+      ("i", "Input path", cxxopts::value<std::string>(img_path));
+  const auto result = options.parse(argc, argv);
+  if (argc == 1 || result.count("help") >= 1) {
+    std::cout << options.help({""});
+    return -1;
+  }
 
-    return 0;
+  auto img = cv::imread(img_path, -1);
+  if (img.empty()) return -1;
+
+  return 0;
 }
